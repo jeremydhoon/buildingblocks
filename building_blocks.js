f3c93343177de419b4b2c237b447205564bad250
Jeremy Hoon <Jeremy.D.Hoon@gmail.com>
2010-10-18 13:11:49 -0400

    Added click handling to remove blocks when they are clicked on. The blocks in the column slide down and fade out the way you would expect.

--- building_blocks.js
+++ building_blocks.js
@@ -16,9 +16,7 @@ var BuildingBlocks = (function() {
 		scope: DRAGGABLE_SCOPE
 	    };
 	    jSrc.draggable(options);
-	    jSrc.bind("selectstart", function(event) {
-		    event.preventDefault();
-		});
+	    disable_select(jSrc);
 	    return jSrc;
 	}
 	
@@ -27,6 +25,7 @@ var BuildingBlocks = (function() {
 		var dx = event.pageX - offset.left;
 		return Math.floor(dx / BLOCK_WIDTH);		
 	    }
+
 	    function drop_handler(event, ui) {
 		var cCol = col_from_event(event);
 		if (cCol >= cColumns || cCol < 0
@@ -35,7 +34,10 @@ var BuildingBlocks = (function() {
 		}
 		
 		var jBlock = $(ui.draggable).clone();
+		disable_select(jBlock);
 		jBlock.hide();
+		jBlock.row = rgRgBlocks[cCol].length;
+		jBlock.col = cCol;
 		jDest.append(jBlock);
 		var cX = cCol * BLOCK_WIDTH;
 		var cY = rgRgBlocks[cCol].length * BLOCK_WIDTH;
@@ -44,27 +46,108 @@ var BuildingBlocks = (function() {
 		
 		rgRgBlocks[cCol].push(jBlock);
 	    }
+
+	    function click_to_rc(event) {
+		var cCol = col_from_event(event);
+		var cRow = Math.floor(((offset.top + cHeight) - event.pageY)
+				      / BLOCK_WIDTH);
+		return {row:cRow, col: cCol};
+	    }
+
+	    function get_block_index(row, col) {
+		var ix = null;
+		$.each(rgRgBlocks[col], function(i,blk) {
+			if (blk.row == row) {
+			    ix = i;
+			}
+		    });
+		return ix;
+	    }
+
+	    function get_block(row, col) {
+		var ix = get_block_index(row,col);
+		return rgRgBlocks[col][ix];
+	    }
+
+	    function remove_block(rc) {
+		var ix = get_block_index(rc.row, rc.col);
+		var block = rgRgBlocks[rc.col][ix];
+		block.fadeOut(100, function() {block.remove();});
+		rgRgBlocks[block.col].splice(ix,1);
+	    }
+
+	    function lower_col(col) {
+		var dictRow = {};
+		var rgBlocks = rgRgBlocks[col];
+		$.each(rgBlocks, function(i,block) {
+			dictRow[block.row] = block;
+		    });
+		var bottom = null;
+		$.each(rgBlocks, function(i,block) {
+			if (block.row > 0
+			    && dictRow[block.row-1] === undefined) {
+			    bottom = block.row;
+			}
+		    });
+		if (bottom === null) {
+		    return;
+		}
+		var toDrop = [];
+		$.each(rgBlocks, function(i,block) {
+			if (block.row >= bottom) {
+			    block.row--;
+			    toDrop.push(block);
+			}
+		    });
+		$.each(toDrop, function(i,block) {
+			block.animate({bottom:"-=" + BLOCK_WIDTH}, 200);
+		    });
+		return toDrop.length;
+	    }
+
+	    function check_rc(rc) {
+		return get_block_index(rc.row, rc.col) !== null;
+	    }
+
+	    function click_handler(event) {
+		var rc = click_to_rc(event);
+		if (check_rc(rc)) {
+		    remove_block(rc);
+		    lower_col(rc.col);
+		}
+	    }
+
 	    var cWidth = jDest.width();
 	    var cHeight = jDest.height();
 	    var cColumns = cWidth / BLOCK_WIDTH;
 	    var cMaxHeight = cHeight / BLOCK_WIDTH;
 	    var offset = jDest.offset();
+	    
 	    var rgRgBlocks = [];
 	    var i;
 	    for (i = 0; i < cColumns; i++) {
 		rgRgBlocks.push([]);
 	    }
+
 	    var options = {
 		drop: drop_handler,
 		activeClass: "droppable_highlight",		
 		scope: DRAGGABLE_SCOPE
 	    };
 	    jDest.droppable(options);
+	    jDest.click(click_handler);
+
 	    var obj = {
 		blocks: rgRgBlocks,
 		widget: jDest
 	    };
 	    return obj;
+	}
+
+	function disable_select(j) {
+	    j.bind("selectstart", function(event) {
+		    event.preventDefault();
+		});
 	}
 
 	function clear_dest(dest) {
